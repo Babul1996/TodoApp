15bad67332669eefafc3da73eec454d5ef689794
Babul Kumar <babulkumargupta123@gmail.com>
2022-01-03 18:59:19 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -1,54 +1,71 @@
 
-import { useState } from 'react';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 import Todo from './Todo';
 
-const App =()=> {
+const App = () => {
 
-  const[item, setitem]=useState('');
-  const[additems,setadditems]=useState([]);
-  const InputEvents=(e)=>{
+  let textInput = null;
+  useEffect(() => {
+    textInput.focus();
+  })
+  const [item, setitem] = useState('');
+  const [additems, setadditems] = useState([]);
+
+  const InputEvents = (e) => {
     setitem(e.target.value);
   }
 
-  const addItem=()=>{
-    setadditems((olddata)=>{
-return[...olddata,item]
+  const addItem = () => {
+    setadditems((olddata) => {
+      return [...olddata, item]
     })
     setitem('')
   }
 
 
-  const deleteItem = (id)=>{
-    setadditems((olddata)=>{
-      return olddata.filter((arrEle,index)=>{
-        return index !==id;
+  const deleteItem = (id) => {
+    setadditems((olddata) => {
+      return olddata.filter((arrEle, index) => {
+        return index !== id;
       })
     })
   }
+
+ const onKeyDownHandler = (e) => {
+    if (e.keyCode === 13) {
+      addItem();
+    }
+  };
+
+
   return (
     <div className="main_div">
-     <div className='center_div'>
-       <br/>
-       <h1>ToDo List</h1>
-       <br/>
-       <input type="text" placeholder="Add a Item"
-       value={item}
-        onChange={InputEvents}
-       />
-       <button onClick={addItem}> + </button>
+      <div className='center_div'>
+        <br />
+        <h1>ToDo List</h1>
+        <br />
+        <input type="text" placeholder="Add a Item" className='check' ref={(text) => { textInput = text; }}
+          value={item}
+          onChange={InputEvents}
+          onKeyDown={onKeyDownHandler}
 
-       <ol>
-         
+        />
+        <button onClick={addItem}> ADD </button>
 
-{
-  additems.map((value,index)=>{
-  return<Todo key={index} id={index} text={value} onSelect={deleteItem}/>
-  })
-}
-        
-       </ol>
-     </div>
+        <ol>
+
+
+          {
+            additems.map((value, index) => {
+              return <Todo key={index} id={index} text={value} onSelect={deleteItem} />
+            })
+          }
+
+
+        </ol>
+      </div>
     </div>
   );
 }
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
+import { useState } from 'react/cjs/react.development'
 
 const Todo = (props) => {
+  const [line, setline] = useState()
 
-    
-    return (
-        <>
-        <div className='todo_style'>
-       <i className='fa fa-times' 
-       onClick={()=>{
-           props.onSelect(props.id)
-       }}/>
-             <li> {props.text} </li>
-             </div>
-        </>
-    );
+  const cutit = () => {
+    setline(!line)
+  }
+  return (
+    <>
+      <div className='todo_style'>
+        <i className='fa fa-trash'
+          onClick={() => {
+            props.onSelect(props.id)
+          }} />
+        <span>
+          <input type="checkbox" onClick={cutit} id='cut' />
+          <li style={{ textDecoration: line ? "line-through" : line }}> {props.text} </li>
+
+        </span>
+      </div>
+    </>
+  );
 }
 
 export default Todo
